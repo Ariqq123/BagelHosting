414e1166558e295b4291bb6a885537d27d349ca6
Ariqq123 <arinata308@gmail.com>
2026-06-04 17:25:05 +0000

    feat: import Cloudflare subdomain domains

--- resources/views/admin/subdomains/index.blade.php
+++ resources/views/admin/subdomains/index.blade.php
@@ -17,6 +17,75 @@
     <div class="col-xs-12">
         <div class="box box-primary">
             <div class="box-header with-border">
+                <h3 class="box-title">Import from Cloudflare</h3>
+            </div>
+            <form action="{{ route('admin.subdomains.import.preview') }}" method="POST">
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="pImportToken" class="control-label">Cloudflare API Token</label>
+                        <input type="password" id="pImportToken" name="cloudflare_token" class="form-control" value="{{ old('cloudflare_token', $importToken ?? '') }}" />
+                        <p class="text-muted small">Token requires <code>Zone:Read</code> and <code>DNS:Edit</code>. Imported zones are enabled with A records; edit a domain after import to add CNAME support.</p>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    {!! csrf_field() !!}
+                    <button type="submit" class="btn btn-sm btn-primary">List Cloudflare Zones</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@if(isset($importZones))
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title">Cloudflare Zones</h3>
+            </div>
+            <form action="{{ route('admin.subdomains.import') }}" method="POST">
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th></th>
+                                <th>Domain</th>
+                                <th>Zone ID</th>
+                                <th>Status</th>
+                            </tr>
+                            @forelse($importZones as $index => $zone)
+                                @php($exists = in_array($zone['name'], $existingDomainNames ?? [], true))
+                                <tr>
+                                    <td class="text-center">
+                                        <input type="checkbox" name="zones[{{ $index }}][selected]" value="1" @if(!$exists) checked @else disabled @endif />
+                                        <input type="hidden" name="zones[{{ $index }}][id]" value="{{ $zone['id'] }}" />
+                                        <input type="hidden" name="zones[{{ $index }}][name]" value="{{ $zone['name'] }}" />
+                                    </td>
+                                    <td>{{ $zone['name'] }}</td>
+                                    <td><code>{{ $zone['id'] }}</code></td>
+                                    <td>{{ $exists ? 'Already imported' : 'Ready' }}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="4" class="text-center">No zones were returned by Cloudflare.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="box-footer">
+                    {!! csrf_field() !!}
+                    <input type="hidden" name="cloudflare_token" value="{{ $importToken }}" />
+                    <button type="submit" class="btn btn-sm btn-success">Import Selected Zones</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endif
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-primary">
+            <div class="box-header with-border">
                 <h3 class="box-title">Domain List</h3>
                 <div class="box-tools">
                     <a class="btn btn-sm btn-primary" href="{{ route('admin.subdomains.create') }}">Create New</a>
