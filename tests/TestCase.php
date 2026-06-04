0a4f27877ab16351fec088a5d60eb89c60674e6d
Ariqq123 <arinata308@gmail.com>
2026-06-04 18:52:34 +0000

    test: add phpunit harness

---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pterodactyl\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('database.default', 'testing');
+        config()->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+            'foreign_key_constraints' => true,
+        ]);
+    }
+}
